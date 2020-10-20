06b442cf9bcd1ea28dfe1dd992a5dd68e20a2c9a
Diego Eiras <eiras.lucio@gmail.com>
2020-10-20 00:39:04 -0300

    📦 ADD: Pages initial state

---
+++ src/pages/Read/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { Container } from './styles'
+
+export default function Read() {
+  return (
+    <Container>
+      <Text>Read</Text>
+    </Container>
+  )
+}
