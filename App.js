c494a8ee2ea588331ec3eda44488a5496ece2db1
Diego Eiras <eiras.lucio@gmail.com>
2020-10-21 14:21:05 -0300

    📦 ADD: Initialize Firebase

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-/* eslint-disable camelcase */
-
 import React from 'react'
+import { LogBox } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { AppLoading } from 'expo'
 
@@ -13,9 +12,29 @@ import {
   Roboto_900Black,
 } from '@expo-google-fonts/roboto'
 
+import firebase from 'firebase'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
 import Routes from './src/Routes'
 
 export default function App() {
+  LogBox.ignoreLogs(['Setting a timer', 'Deprecated'])
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: process.env.EXPO_FIREBASE_API_KEY,
+      authDomain: process.env.EXPO_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.EXPO_FIREBASE_DATABASE_URL,
+      projectId: process.env.EXPO_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.EXPO_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.EXPO_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.EXPO_FIREBASE_APP_ID,
+      measurementId: process.env.EXPO_FIREBASE_MEASUREMENT_ID,
+    });
+  }
+
   const [fontsLoaded] = useFonts({
     Roboto_300Light,
     Roboto_400Regular,
@@ -31,7 +50,7 @@ export default function App() {
   return (
     <>
       <Routes />
-      <StatusBar style="dark" />
+      <StatusBar style="dark" backgroundColor="#FAFAFA" translucent={false} />
     </>
   )
 }
