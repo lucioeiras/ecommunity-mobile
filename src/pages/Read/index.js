cd64019b44b6b86b84ef1318cd940eb6a9a0001c
Diego Eiras <eiras.lucio@gmail.com>
2020-10-21 14:33:18 -0300

    📦 ADD: Render post info

--- src/pages/Read/index.js
+++ src/pages/Read/index.js
@@ -1,12 +1,93 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
 
-import { Container } from './styles'
+import avatar from '../../assets/eu.jpeg'
+import firebaseImg from '../../assets/firebase.png'
 
-export default function Read() {
+import { 
+  Container as Content, 
+  Title, 
+  Subtitle, 
+  Hint,
+  Gradient, 
+} from '../../styles/global'
+
+import { 
+  Container,
+  Header,
+  Profile,
+  Author,
+  Avatar,
+  User,
+  PostText,
+  LinkList,
+  Link,
+  Linkname,
+  FilesList,
+  File,
+  Filename,
+} from './styles'
+
+export default function Read({ route }) {
+  const { post, user } = route.params
+
   return (
     <Container>
-      <Text>Read</Text>
+      {post && (
+        <>
+        <Header source={{ uri: post.thumbURL }}>
+          <Gradient colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.7)']}>
+            <Profile>
+              <Avatar source={{ uri: user.avatar }} />
+              {user && (
+                <Author>
+                  <User>{user.name}</User>
+                  <Hint textColor="#FFF">Autor do texto</Hint>
+                </Author>
+              )}
+            </Profile>
+          </Gradient>
+        </Header>
+
+        <Content>
+          <Title>{post.title}</Title>
+          <PostText>{post.text}</PostText>
+
+          {!!post.links && !!post.links[0] && (
+            <>
+              <Subtitle>Links</Subtitle>
+              <LinkList>
+                {post.links.map(link => (
+                  <Link key={link}>
+                    <MaterialIcons name="public" color="#656565" size={20} />
+                    <Linkname>{link}</Linkname>
+                  </Link>
+                ))}
+              </LinkList>
+            </>
+          )}
+
+          {!!post.filesURL && !!post.filesURL[0] && (
+            <>
+              <Subtitle>Arquivos</Subtitle>
+              <FilesList>
+                {post.filesURL.map((file, index) => (
+                  <File key={index}>
+                    <MaterialIcons 
+                      name="insert-drive-file" 
+                      color="#656565" 
+                      size={48} 
+                    />
+                    <Filename>Arquivo {index + 1}</Filename>
+                  </File>
+                ))}
+              </FilesList>
+            </>
+          )}
+        </Content>
+        </>
+      )}
     </Container>
   )
 }
--- src/pages/Read/styles.js
+++ src/pages/Read/styles.js
@@ -1,5 +1,96 @@
 import styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   flex: 1;
+  background: #FAFAFA;
+`
+
+export const Header = styled.ImageBackground`
+  height: 327px;
+  margin-bottom: 32px;
+`
+
+export const Profile = styled.View`
+  flex-direction: row;
+`
+
+export const Avatar = styled.Image`
+  width: 64px;
+  height: 64px;
+
+  overflow: hidden;
+  border-radius: 5px;
+
+  margin-right: 16px;
+`
+
+export const Author = styled.View`
+  height: 64px;
+  justify-content: center;
+`
+
+export const User = styled.Text`
+  color: #FFF;
+  font-size: 20px;
+  font-family: 'Roboto_500Medium';
+`
+
+export const PostText = styled.Text`
+  color: #737380;
+  font-size: 16px;
+  font-family: 'Roboto_400Regular';
+  line-height: 25px;
+  text-align: justify;
+
+  margin-bottom: 32px;
+`
+
+export const LinkList = styled.View`
+  margin-bottom: 24px;
+`
+
+export const Link = styled(RectButton)`
+  height: 48px;
+
+  flex-direction: row;
+  align-items: center;
+
+  border-radius: 5px;
+  background: #EAEAEA;
+
+  padding: 0 16px;
+  margin-bottom: 12px;
+`
+
+export const Linkname = styled.Text`
+  color: #656565;
+  font-size: 16px;
+  font-family: 'Roboto_500Medium';
+
+  margin-left: 8px;
+`
+
+export const FilesList = styled.View`
+  flex-direction: row;
+  margin-bottom: 32px;
+`
+
+export const File = styled(RectButton)`
+  border-radius: 5px;
+  background: #EAEAEA;
+
+  align-items: center;
+  justify-content: center;
+
+  padding: 20px;
+  margin-right: 12px;
+`
+
+export const Filename = styled.Text`
+  color: #656565;
+  font-size: 16px;
+  font-family: 'Roboto_500Medium';
+
+  margin-top: 8px;
 `
