9c73212bbae654ca34f95175edd6940792b5e620
Diego Eiras <eiras.lucio@gmail.com>
2020-10-21 14:32:43 -0300

    📦 ADD: Load last posts and spotlight post from Firestore

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,83 +1,88 @@
-import React from 'react'
-import { MaterialIcons } from '@expo/vector-icons'
+import React, { useState, useEffect } from 'react'
+import { View } from 'react-native'
+
+import firebase from 'firebase/app'
 
 import Header from '../../components/Header'
 import Spotlight from '../../components/Spotlight'
 import Post from '../../components/Post'
+import SearchBar from '../../components/SearchBar'
 
-import avatar from '../../assets/eu.jpeg'
-import firebaseImg from '../../assets/firebase.png'
+import { Container, Title, Subtitle } from '../../styles/global'
 
 import { 
-  Container,
   SearchContainer,
-  SearchBar,
-  Input,
-
-  Title,
-  Subtitle,
-
   SpotlightContainer,
-
   LastContainer,
-  LastPostsList,
 } from './styles'
 
 export default function Home() {
+  const firestore = firebase.firestore()
+  const postsRef = firestore.collection('posts')
+
+  const [lastPosts, setLastPosts] = useState()
+  const [spotlightPost, setSpotlightPost] = useState()
+
+  useEffect(() => {
+    postsRef
+      .orderBy('clicks', 'desc')
+      .limit(1)
+      .get()
+      .then(querySnapshot => {
+        const searchedPosts = []
+
+        querySnapshot.forEach(doc => searchedPosts.push(doc.data()))
+        
+        setSpotlightPost(searchedPosts[0])
+      })
+  }, [])
+
+  useEffect(() => {
+    postsRef
+      .orderBy('createdAt', 'desc')
+      .limit(20)
+      .get()
+      .then(querySnapshot => {
+        const searchedPosts = []
+
+        querySnapshot.forEach(doc => searchedPosts.push(doc.data()))
+        
+        setLastPosts(searchedPosts)
+      })
+  }, [])
+
   return (
     <Container>
-      <Header 
-        user="Diego Eiras"
-        avatar={avatar}
-      />
+      <Header />
 
       <SearchContainer>
         <Title>Encontre um novo conhecimento</Title>
-        <SearchBar>
-          <MaterialIcons name="search" color="#A8A8B3" size={24} />
-          <Input placeholder="Pesquise algo interessante" />
-        </SearchBar>
+        <SearchBar />
       </SearchContainer>
 
       <SpotlightContainer>
         <Subtitle>Destaque</Subtitle>
 
-        <Spotlight 
-          thumb={firebaseImg}
-          title="O que é Firebase?"
-          avatar={avatar}
-          user="Diego Eiras"
-        />
+        {spotlightPost && (
+          <Spotlight 
+            post={spotlightPost}
+            userID={spotlightPost.user_id}
+          />
+        )}
       </SpotlightContainer>
 
       <LastContainer>
         <Subtitle>Últimas adicionadas</Subtitle>
 
-        <LastPostsList>
-          <Post 
-            thumb={firebaseImg}
-            title="O que é firebase?"
-            user="Diego Eiras"
-          />
-
-          <Post 
-            thumb={firebaseImg}
-            title="O que é firebase?"
-            user="Diego Eiras"
-          />
-
-          <Post 
-            thumb={firebaseImg}
-            title="O que é firebase?"
-            user="Diego Eiras"
-          />
-
-          <Post 
-            thumb={firebaseImg}
-            title="O que é firebase?"
-            user="Diego Eiras"
-          />
-        </LastPostsList>
+        <View>
+          {!!lastPosts && lastPosts.map(post => (
+            <Post 
+              key={post.uid}
+              post={post}
+              userID={post.user_id}
+            />  
+          ))}
+        </View>
       </LastContainer>
     </Container>
   )
